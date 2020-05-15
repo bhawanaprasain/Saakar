e3fd2e89ad74d571162ca485d249121cbd9a8719
bhawana <bhawana.prs@gmail.com>
2020-05-15 13:34:45 +0545

    Post job details to database

--- src/components/JobForm.js
+++ src/components/JobForm.js
@@ -6,42 +6,52 @@ class JobForm extends Component{
         super()
 
         this.state = {
-            title: ",jkjkjk",
-            description:"khjhjkhkjh",
-            category:"jljkj",
-            budget:"50000",
+            title: "",
+            description:"",
+            category:"",
+            budget:"",
+            location: "",
+            description: "",
 
-            user: "bhawana",
+          
         }
-        this.handleSubmit = this.handleSubmit.bind(this)
+
+        this.handleInput = this.handleInput.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this) 
+       }
+
+       handleInput(e){
+        this.setState({
+            [e.target.name]: e.target.value
+    
+        })
+
+    
     }
     handleSubmit(){
-        console.log("wow")
-       
-
         
-            const jobdata = {
-                title: this.state.title,
-                description : this.state.description,
-                category: this.state.category,
-                budget: this.state.budget
-     
-             }
+        
+        const jobdata ={
+            title: this.state.title,
+            description:this.state.description,
+            category:this.state.category,
+            budget:this.state.budget,
+            location: this.state.location,
+            description: this.state.description,
 
-     
-       
-
+        }
+        
         axios({
             method: "post",
-            url: "localhost:3000/create",
+            url: "",
             data: jobdata
 
         }).then((response)=>{
-            console.log(response);
+            alert(response);
             
         })
         .catch(function(error){
-            console.log(error)
+            alert(error)
         })
     }
 
@@ -49,84 +59,77 @@ class JobForm extends Component{
     render(){
         return(
             <div>
-                <div class="ftco-section bg-light">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-12 col-lg-8 mb-5">
-                                    <form action="#" class="p-5 bg-white">
-                                        <div class="row form-group">
-                                            <div class="col-md-12 mb-3 mb-md-0">
-                                                <label class="font-weight-bold" for="fullname">Job Title</label>
-                                                <input type="text" id="fullname" class="form-control" placeholder="eg. Professional UI/UX Designer" />
-                                            </div>
-                                        </div>
-
-                                        <div class="row form-group mb-5">
-                                            <div class="col-md-12 mb-3 mb-md-0">
-                                                <label class="font-weight-bold" for="fullname">Company</label>
-                                                <input type="text" id="fullname" class="form-control" placeholder="eg. Facebook, Inc." />
+                <div className="ftco-section bg-light">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12 col-lg-8 mb-5">
+                                    <form action="#" className="p-5 bg-white">
+                                        <div className="row form-group">
+                                            <div className="col-md-12 mb-3 mb-md-0">
+                                                <label className="font-weight-bold" htmlFor="fullname">Job Title</label>
+                                                <input type="text" id="fullname" className="form-control" placeholder="eg. Professional UI/UX Designer"  name="title" onChange={this.handleInput} />
                                             </div>
                                         </div>
 
 
-                                <div class="row form-group">
-                                    <div class="col-md-12"><h3>Job Type</h3></div>
-                                        <div class="col-md-12 mb-3 mb-md-0">
-                                            <label for="option-job-type-1">
-                                                <input type="radio" id="option-job-type-1" name="job-type" /> Full Time
+                                <div className="row form-group">
+                                    <div className="col-md-12"><h3>Job Type</h3></div>
+                                        <div className="col-md-12 mb-3 mb-md-0">
+                                            <label htmlFor="option-job-type-1">
+                                                <input type="radio" id="option-job-type-1" name="job-type" name="category" onChange={this.handleInput}/> Full Time
                                             </label>
                                         </div>
-                                        <div class="col-md-12 mb-3 mb-md-0">
-                                            <label for="option-job-type-2">
-                                                <input type="radio" id="option-job-type-2" name="job-type" /> Part Time
+                                        <div className="col-md-12 mb-3 mb-md-0">
+                                            <label htmlFor="option-job-type-2">
+                                                <input type="radio" id="option-job-type-2" name="job-type" name="category" onChange={this.handleInput}/> Part Time
                                             </label>
                                         </div>
 
-                                        <div class="col-md-12 mb-3 mb-md-0">
-                                        <label for="option-job-type-3">
-                                            <input type="radio" id="option-job-type-3" name="job-type" /> Freelance
+                                        <div className="col-md-12 mb-3 mb-md-0">
+                                        <label htmlFor="option-job-type-3">
+                                            <input type="radio" id="option-job-type-3" name="job-type" name="category" onChange={this.handleInput}/> Freelance
                                         </label >
                                         </div>
 
-                                        <div class="col-md-12 mb-3 mb-md-0">
-                                            <label for="option-job-type-4">
-                                                <input type="radio" id="option-job-type-4" name="job-type" /> Internship
+                                        <div className="col-md-12 mb-3 mb-md-0">
+                                            <label htmlFor="option-job-type-4">
+                                                <input type="radio" id="option-job-type-4" name="job-type" name="category" onChange={this.handleInput}/> Internship
                                             </label>
                                         </div>
 
-                                        <div class="col-md-12 mb-3 mb-md-0">
-                                            <label for="option-job-type-4">
-                                                <input type="radio" id="option-job-type-4" name="job-type" /> Temporary
+                                        <div className="col-md-12 mb-3 mb-md-0">
+                                            <label htmlFor="option-job-type-4">
+                                                <input type="radio" id="option-job-type-4" name="job-type" name="category" onChange={this.handleInput}/> Temporary
                                             </label>
                                         </div>
 
                                 </div>
 
-                                <div class="row form-group mb-4">
-                                    <div class="col-md-12"><h3>Location</h3></div>
-                                        <div class="col-md-12 mb-3 mb-md-0">
-                                            <input type="text" class="form-control" placeholder="Pokhara" />
+                                <div className="row form-group mb-4">
+                                    <div className="col-md-12"><h3>Location</h3></div>
+                                        <div className="col-md-12 mb-3 mb-md-0">
+                                            <input type="text" className="form-control" placeholder="Pokhara" name="location" onChange={this.handleInput}/>
                                         </div>
                                 </div>
 
-                                <div class="row form-group mb-4">
-                                    <div class="col-md-12"><h3>Budget</h3></div>
-                                        <div class="col-md-12 mb-3 mb-md-0">
-                                            <input type="number" class="form-control" placeholder="" />
+                                <div className="row form-group mb-4">
+                                    <div className="col-md-12"><h3>Budget</h3></div>
+                                        <div className="col-md-12 mb-3 mb-md-0">
+                                            <input type="number" className="form-control" placeholder="" name="budget" onChange={this.handleInput}/>
                                         </div>
                                 </div>
                                 
 
-                                <div class="row form-group">
-                                    <div class="col-md-12"><h3>Job Description</h3></div>
-                                        <div class="col-md-12 mb-3 mb-md-0">
-                                            <textarea name="" class="form-control" id="" cols="30" rows="5"></textarea>
+                                <div className="row form-group">
+                                    <div className="col-md-12"><h3>Job Description</h3></div>
+                                        <div className="col-md-12 mb-3 mb-md-0">
+                                            <textarea name="" className="form-control" id="" cols="30" rows="5" name="description" onChange={this.handleInput}></textarea>
                                         </div>
                                 </div>
 
-                                <div class="row form-group">
-                                        <div class="col-md-12">
-                                            <input type="submit" value="Post" class="btn btn-primary  py-2 px-5" />
+                                <div className="row form-group">
+                                        <div className="col-md-12">
+                                            <input type="submit" value="Post" className="btn btn-primary  py-2 px-5" onClick={this.handleSubmit} />
                                         </div>
                                 </div>
 
